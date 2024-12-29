e5452136e598676ce60ba0f0e8af3e7cf6b65df4
Jaimin Chovatia <chovatiajaimin@gmail.com>
2024-12-29 12:46:13 +0530

    feat: loader in settings

--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -1,10 +1,22 @@
+'use client';
+
 import React from 'react';
 import { Card, CardHeader, CardTitle } from '../ui/card';
 import AccountDetailsForm from '../Forms/Settings/account-details-form';
 import Preferences from '../Forms/Settings/preferences';
 import OtherDetails from './other-details';
+import Loader from '../Layout/loader';
+import { useUser } from '@/app/_providers/user-provider';
+import { useUserStore } from '@/store/userStore';
 
 export default function SettingsComponent() {
+  const { user: authUser } = useUser();
+  const user = useUserStore((state) => state.user);
+
+  if (!authUser && !user) {
+    return <Loader />;
+  }
+
   return (
     <section className="flex flex-1 justify-center space-y-4 overflow-auto p-4 py-4 lg:px-16">
       <div className="w-full max-w-sm space-y-4">
